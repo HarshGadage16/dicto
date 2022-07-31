1540edaa9b48229cd67d9498e8a438def8f4c54a
Harsh Gadage <62607923+HarshGadage16@users.noreply.github.com>
2022-07-31 13:22:58 +0530

    adding footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,19 +7,19 @@ const Footer = () => {
       <hr style={{ width: "90%", marginTop: 20 }} />
       <span className="name">
         Made by{" "}
-        <a href="https://www.youtube.com/c/RoadsideCoder" target="__blank">
-          Roadside Coder
+        <a href="https://github.com/HarshGadage16" target="__blank">
+          Harsh Gadage
         </a>
       </span>
       <div className="iconContainer">
-        <a href="https://www.instagram.com/roadsidecoder/" target="__blank">
+        <a href="https://www.instagram.com/harsh._.1.6/" target="__blank">
           <i className="fab fa-instagram-square fa-2x"></i>
         </a>
-        <a href="https://www.linkedin.com/in/piyush-eon" target="__blank">
+        <a href="linkedin.com/in/harsh-gadage/" target="__blank">
           <i className="fab fa-linkedin fa-2x"></i>
         </a>
-        <a href="https://www.youtube.com/c/RoadsideCoder" target="__blank">
-          <i className="fab fa-youtube fa-2x"></i>
+        <a href="https://github.com/HarshGadage16" target="__blank">
+          <i className="fab fa-github fa-2x"></i>
         </a>
       </div>
     </div>
